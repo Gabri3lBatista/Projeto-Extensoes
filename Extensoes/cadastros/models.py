c85cb05c5137439ef9eeba7b551b2f93fc63fd47
Clivia Bayum <clivia.bayum@gmail.com>
2022-02-11 18:56:38 -0500

    novas atualizaçoes

--- Extensoes/cadastros/models.py
+++ Extensoes/cadastros/models.py
@@ -4,7 +4,7 @@ class Grupos(models.Model):
     grupo = models.CharField('Nome do Grupo', max_length=200)
     projeto= models.CharField('Nome do Projeto', max_length=200)
     cliente = models.CharField('Nome do Cliente', max_length=200)
-    n_de_part = models.DecimalField('Número de Participantes', max_digits=100, decimal_places=10)
+    n_de_part = models.DecimalField('Número de Participantes', max_digits=100, decimal_places=0)
     descricao = models.CharField('Descrição do Projeto', max_length=200)
     tutorial = models.CharField('tutorial', max_length=200)
     ver = models.CharField('ver', max_length=200)
