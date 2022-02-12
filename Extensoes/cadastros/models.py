7dbe80daa010eec3d342146a7d0ff04fc2d6fefb
Clivia Bayum <clivia.bayum@gmail.com>
2022-02-11 19:19:10 -0500

    listagem e editar

--- Extensoes/cadastros/models.py
+++ Extensoes/cadastros/models.py
@@ -6,11 +6,11 @@ class Grupos(models.Model):
     cliente = models.CharField('Nome do Cliente', max_length=200)
     n_de_part = models.DecimalField('Número de Participantes', max_digits=100, decimal_places=0)
     descricao = models.CharField('Descrição do Projeto', max_length=200)
-    tutorial = models.CharField('tutorial', max_length=200)
     ver = models.CharField('ver', max_length=200)
     status = models.CharField('status', max_length=200)
     dataIni = models.DateField('Data Inicio', blank=True)
     dataFim = models.DateField('Data Fim',blank=True)
+    tutorial = models.CharField('tutorial', max_length=200)
     
     def __str__(self):
         return self.grupo
